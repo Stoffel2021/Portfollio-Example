c07e6ecde3bce95d71ef937c047f3c38e72b4573
Leon O <leon.oberheim@gmail.com>
2022-03-30 13:21:43 +0100

    version is when the video finishes the setting up the sections chapter

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
 
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
-
-
-function PageTransitions(){
-    //Button click active class
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-            console.log("test");
-        })
+function PageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
+      this.className += " active-btn";
+      console.log("test");
+    });
+  }
+  //Sections active class, use to change the section when selected
+  allSections.addEventListener("click", (e) => {
+    // e is the event that targets the control class data set
+    const id = e.target.dataset.id;
+    if (id) {
+      //remove selected from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
     }
+  });
 }
 
 PageTransitions();
